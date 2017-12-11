58b5a5ecb9742aabdc315335d3c6608a1153b3df
Joshua Meier <jmeier@e1z3r8p26.42.us.org>
2017-12-10 20:43:16 -0800

    Crying

--- src/ll_cast.c
+++ src/ll_cast.c
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 10:59:06 by jmeier            #+#    #+#             */
-/*   Updated: 2017/12/10 01:41:14 by jmeier           ###   ########.fr       */
+/*   Updated: 2017/12/10 20:43:04 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ll_int_cast(va_list arg)
 	long long			output;
 
 	output = (long long)va_arg(arg, void*);
-	ft_putlonglong(output, 10, 1);
+	ft_putnbr_base_fd((int)output, 10, 1);
 }
 
 void	ll_oct_cast(va_list arg)
@@ -25,7 +25,7 @@ void	ll_oct_cast(va_list arg)
 	unsigned long long	output;
 
 	output = (unsigned long long)va_arg(arg, void*);
-	ft_putulonglong(output, 8, 1);
+	ft_putunbr_base_fd((unsigned int)output, 8, 1);
 }
 
 void	ll_hex_cast(va_list arg)
@@ -33,7 +33,7 @@ void	ll_hex_cast(va_list arg)
 	unsigned long long	output;
 
 	output = (unsigned long long)va_arg(arg, void*);
-	ft_putulonglong(output, 16, 1);
+	ft_putunbr_base_fd((unsigned int)output, 16, 1);
 }
 
 void	ll_lhex_cast(va_list arg)
@@ -41,7 +41,7 @@ void	ll_lhex_cast(va_list arg)
 	unsigned long long	output;
 
 	output = (unsigned long long)va_arg(arg, void*);
-	ft_putlonglong_lc(output, 16, 1);
+	ft_putunbr_base_lc((unsigned int)output, 16, 1);
 }
 
 void	ll_uint_cast(va_list arg)
