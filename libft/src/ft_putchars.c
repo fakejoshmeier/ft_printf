f7d3042e563e9288d263de299f60b3dcdedb54a2
Joshua Meier <jmeier@e1z3r8p26.42.us.org>
2017-12-10 21:30:55 -0800

    Help

--- libft/src/ft_putchars.c
+++ libft/src/ft_putchars.c
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 14:09:59 by jmeier            #+#    #+#             */
-/*   Updated: 2017/12/10 21:29:07 by jmeier           ###   ########.fr       */
+/*   Updated: 2017/12/10 21:30:29 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,4 @@ void	ft_putchars(char c, int amt)
 	i = -1;
 	while (++i <= amt)
 		write(1, &c, 1);
-}
-
-int		main(void)
-{
-	char c = 'i';
-
-	ft_putchars(c, 5);
-	ft_putchar('\n');
-	return (0);
 }
