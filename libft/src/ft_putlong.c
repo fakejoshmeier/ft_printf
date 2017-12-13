ebd8feb6fbe187d409b465d715cdfdcc9c517c38
Joshua Meier <jmeier@e1z3r8p26.42.us.org>
2017-12-12 19:06:19 -0800

    test

--- libft/src/ft_putlong.c
+++ libft/src/ft_putlong.c
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 12:52:12 by jmeier            #+#    #+#             */
-/*   Updated: 2017/12/02 20:40:22 by jmeier           ###   ########.fr       */
+/*   Updated: 2017/12/12 19:06:10 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ void	ft_putlong(long n, long base, int fd)
 	c = "0123456789ABCDEFHIJKLMNOPQRSTUVWXYZ";
 	while ((n / p) >= base || (n / p) <= -base)
 		p *= base;
-	if (n < 0)
-		ft_putchar_fd('-', fd);
 /*
 **	base == 2 ? ft_putstr_fd("0b", fd) : 0;
 **	base == 8 ? ft_putstr_fd("0o", fd) : 0;
