e8c42476c35e5123e48c210d4a43699307841ede
Joshua Meier <jmeier@e1z3r4p23.42.us.org>
2018-03-25 23:15:17 -0700

    Changed strdup to include a failure condition.  Geez.

--- libft/src/ft_strdup.c
+++ libft/src/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 14:40:19 by jmeier            #+#    #+#             */
-/*   Updated: 2017/09/24 23:09:13 by jmeier           ###   ########.fr       */
+/*   Updated: 2018/03/25 23:14:37 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 char	*ft_strdup(const char *src)
 {
-	char	*str;
+	char	*newstr;
+	int		j;
 
-	str = (char*)malloc(ft_strlen(src) + 1);
-	if (str != NULL)
-		ft_strcpy(str, src);
-	return (str);
+	j = 0;
+	if (!(newstr = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1))))
+		return (NULL);
+	while (*str != '\0')
+		newstr[j++] = *str++;
+	newstr[j] = '\0';
+	return (newstr);
 }
