42afc9c60fa06a2d89866354a3eb8c2527f4b315
Joshua Meier <joshua.meier2@gmail.com>
2018-10-22 19:33:05 -0700

    Random Hex fix

--- libft/src/ft_random_hex.c
+++ libft/src/ft_random_hex.c
@@ -6,13 +6,36 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/22 16:25:23 by jmeier            #+#    #+#             */
-/*   Updated: 2018/10/22 16:27:05 by jmeier           ###   ########.fr       */
+/*   Updated: 2018/10/22 19:32:28 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-char		*random_hex(int size)
+char		*ft_str_to_hex(char *s)
+{
+	char		*hex_string;
+	char		*hex;
+	int			len;
+	int			i;
+
+	if ((len = ft_strlen(s)) == 0)
+		return (0);
+	len *= 2;
+	if (!(hex_string = ft_strnew(len)))
+		return (NULL);
+	i = 0;
+	hex = "0123456789ABCDEF";
+	while (i < len)
+	{
+		hex_string[i++] = hex[((*s) >> 4) & 0xF];
+		hex_string[i++] = hex[(*s) & 0xF];
+		++s;
+	}
+	return (hex_string);
+}
+
+char		*ft_random_hex(int size)
 {
 	char		*ret;
 	char		buf[2 + (size / 2)];
