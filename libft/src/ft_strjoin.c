c9f6a3dec260526e27240ae197d7e9c77683b1a6
Joshua Meier <joshua.meier2@gmail.com>
2019-03-21 18:42:54 -0700

    Revamped

--- libft/src/ft_strjoin.c
+++ libft/src/ft_strjoin.c
@@ -6,32 +6,25 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 16:10:46 by jmeier            #+#    #+#             */
-/*   Updated: 2017/09/24 23:13:29 by jmeier           ###   ########.fr       */
+/*   Updated: 2019/03/21 18:41:35 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *a, char const *b)
 {
-	char	*str;
-	int		len1;
-	int		len2;
-	int		a;
-	int		z;
+	char		*c;
+	unsigned	len;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len1 = (int)ft_strlen(s1);
-	len2 = (int)ft_strlen(s2);
-	str = ft_strnew(len1 + len2);
-	if (!str)
-		return (NULL);
-	a = -1;
-	z = -1;
-	while (++a < len1)
-		*(str + a) = *(s1 + a);
-	while (++z < len2)
-		*(str + a++) = *(s2 + z);
-	return (str);
+	len = (a ? ft_strlen(a) : 0) + (b ? ft_strlen(b) : 0);
+	NULL_GUARD(c = malloc(len + 1));
+	c[len] = '\0';
+	if (a)
+		while (*a)
+			*c++ = *a++;
+	if (b)
+		while (*b)
+			*c++ = *b++;
+	return (c - len);
 }
